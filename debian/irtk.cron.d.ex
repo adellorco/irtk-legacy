f29faaa54c94fa825c53d15e6bbd7a0fca882c7f
Ghislain Antony Vaillant <ghisvail@gmail.com>
2015-03-17 14:10:45 +0000

    d/*: remove dh-make examples

--- debian/irtk.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the irtk package
-#
-0 4	* * *	root	[ -x /usr/bin/irtk_maintenance ] && /usr/bin/irtk_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(irtk):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="irtk" command="/usr/bin/irtk"
